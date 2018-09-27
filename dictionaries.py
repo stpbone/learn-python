2fb59f11ab151a2107c724bc0bdbe528d6ae0904
Simon Bone <32798419+stpbone@users.noreply.github.com>
2018-09-27 12:14:48 +0100

    Updated dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,11 +1,55 @@
-thisdict =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+# Create a dictionary with keys and values
+winterbike = {
+  "brand": "Trek",
+  "model": "Madone",
+  "year": 2014
 }
-print(thisdict)
 
-x = thisdict.get("model")
+summerbike = {
+    "brand": "BMC",
+    "color": "Black",
+    "type": "Road",
+    "year": 2011
+}
 
-print(x)
+trackbike = {
+    "brand": "Dolan",
+    "color": "Black",
+    "type": "Track",
+    "year": 2011
+}
 
+mountainbike = {
+    "brand": "Specialized",
+    "color": "Orange",
+    "type": "Mountain",
+    "year": 2012
+}
+
+# Use 'get' function to get the values of specificed keys within the dictionary
+y = winterbike.get("year")
+
+a = summerbike.get("brand")
+b = summerbike.get("type")
+
+c = trackbike.get("type")
+
+print(y)
+
+print(a)
+print(b)
+print(c)
+
+# print values for all keys in the dictionary
+for x in winterbike:
+    print(x)
+
+for x in trackbike:
+    print(x)
+
+# get all values in a specified dictionary
+d = winterbike.values()
+print(d)
+
+e = mountainbike.values()
+print(e)
